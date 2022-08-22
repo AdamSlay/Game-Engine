8b82117925085d488c15e6412a985dd027f62c46
Adam Slay <adamslay11@gmail.com>
2022-08-21 22:21:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My First Game Engine from Scratch
+
+In this repo I will be working on my first game engine in C++. This will
+mainly be for experimentation and learning. To begin with, I will be
+familiarizing myself with the tools needed to build an engine, like libraries
+for rendering to the screen. The first file is pixel.cpp in which I am attempting
+to print pixels to the screen using C++ and SDL.
---
+++ pixel.cpp
@@ -0,0 +1,80 @@
+// Rendering a Pixel to the screen.
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <cmath>
+
+int main()
+{ 
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+    
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    // Ground 
+    SDL_RenderDrawLine(renderer, 0, 400, 640, 400);
+    int c = 4;
+    int g_tex = 0;
+    int ground = 400;
+    int b_window = 480;
+    while (g_tex < 700)
+    { 
+        SDL_RenderDrawLine(renderer, g_tex, b_window, g_tex + 50, ground + 12);
+        g_tex += 50;
+    }
+
+    // Wall
+    int L_wall = 100;
+    int R_wall = 530;
+    int T_wall = 300;
+    SDL_RenderDrawLine(renderer, L_wall, T_wall, L_wall, ground);
+    SDL_RenderDrawLine(renderer, R_wall, T_wall, R_wall, ground);
+    int x = L_wall;
+    while (x < R_wall)
+    {
+        if (x % 20 == 0)
+        {
+            // Horizontal Upper
+            // Top of Wall
+            SDL_RenderDrawLine(renderer, x, T_wall, x + 10, T_wall);    
+            // Bricks
+            for (int y = T_wall + 12.5; y < ground; y += 10)
+            {
+                SDL_RenderDrawLine(renderer, x, y, x + 10, y);
+            }
+        }
+        else if (x % 10 == 0) 
+        {
+            // Horizontal Lower
+            // Top of Wall
+            SDL_RenderDrawLine(renderer, x, T_wall + 10, x + 10, T_wall + 10);
+            // Bricks
+            for (int y = T_wall + 17.5; y < ground; y += 10)
+            {
+                SDL_RenderDrawLine(renderer, x, y, x + 10, y);
+            }
+        }
+        // Top of Wall - vert
+        SDL_RenderDrawLine(renderer, x + 10, T_wall, x + 10, T_wall + 10);
+        SDL_RenderDrawLine(renderer, L_wall, T_wall + 12.5, R_wall, T_wall + 12.5);
+       
+        // Bricks - vert 
+        SDL_RenderDrawLine(renderer, x + 10, T_wall+12.5, x + 10, ground);
+
+        x += 10; 
+    }
+
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+
+    return 0;
+}
